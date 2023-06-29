8dededabba953ac1c20c37bbb86e86cb51c3396e
DCtheTall <dylancutler@google.com>
2023-06-29 12:55:43 -0400

    Reduce to minimal code needed to reproduce

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
 const puppeteer = require("puppeteer");
+
 const url = "https://twitter.com/i/flow/login";
-const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36';
-const viewPort = {
-    width: 1440,//px
-    height: 812//px
-};
-const timeout = 30 * 1e3; // 30 seconds in ms.
+const timeout = 5 * 1e3; // 5 seconds.
 
 (async () => {
     const browser = await puppeteer.launch({
         headless: 'new',
-        args: [
-            // enable FLoC
-            '--enable-blink-features=InterestCohortAPI',
-            '--enable-features="FederatedLearningOfCohorts:update_interval/10s/minimum_history_domain_size_required/1,FlocIdSortingLshBasedComputation,InterestCohortFeaturePolicy"',
-            '--js-flags="--async-stack-traces --stack-trace-limit 32"',
-            '--show-autofill-signatures'
-        ],
+        args: [],
         devtools: true,
     });
 
@@ -34,8 +24,6 @@ const timeout = 30 * 1e3; // 30 seconds in ms.
     });
 
     const page = await ctx.newPage();
-    await page.setUserAgent(userAgent);
-    await page.setViewport(viewPort);
 
     const cdpClient = await page.target().createCDPSession();
     await cdpClient.send('Target.setAutoAttach', {autoAttach: true, waitForDebuggerOnStart: true});
